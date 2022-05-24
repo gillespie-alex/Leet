1ffdbae231da39c3d6f5ee38d4b12f9c0808599e
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-05-24 16:59:39 -0700

    Time: 101 ms (18.56%), Space: 14.6 MB (60.89%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -20,17 +20,3 @@ class Solution(object):
                
         
         
-# low = 0
-#         high = len(nums)-1
-#         return self.bin_search( nums, low, high, target)
-#     def bin_search(self, arr, low, high, x):
-#         if high>= low:
-#             mid = (high + low)//2
-#             if arr[mid] == x:
-#                 return mid
-#             elif arr[mid] > x:
-#                 return self.bin_search(arr, low, mid -1, x)
-#             else:
-#                 return self.bin_search(arr, mid + 1, high, x)
-#         else:
-#             return -1

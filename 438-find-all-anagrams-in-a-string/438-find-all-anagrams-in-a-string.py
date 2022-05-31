a559f09d3ca3ad65af55b689fda1d2033d548ccb
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-05-31 13:44:18 -0700

    Time: 171 ms (61.57%), Space: 15.3 MB (32.83%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -4,23 +4,27 @@ class Solution:
         if original_len < check_len:
             return []
 
-        res = []
+        ans = []
         # stores the frequency of each character in the check string
-        check_counter = [ 0 ] * 26
+        check_cnt = [ 0 ] * 26
         # stores the frequency of each character in the current window
         window = [ 0 ] * 26
         a = ord('a')  # ascii value of 'a'
         # first window
         for i in range(check_len):
-            check_counter[ord(check[i]) - a] += 1
+            #this fills our base dictionary
+            check_cnt[ord(check[i]) - a] += 1
             window[ord(original[i]) - a] += 1
-        if window == check_counter:
-            res.append(0)
-
-        for i in range(check_len, original_len):
-            window[ord(original[i - check_len]) - a] -= 1
-            window[ord(original[i]) - a] += 1
-            if window == check_counter:
-                res.append(i - check_len + 1)
-        return res
+        if check_cnt == window:
+            ans.append(0)
         
+        #sliding window
+        #window moves from i to i + check_len
+        for i in range(1, original_len - check_len + 1):
+            #clear the first element of previous window
+            window[ord(original[i - 1]) - a] -= 1
+            #add new element to the window
+            window[ord(original[i + (check_len - 1)]) - a] += 1
+            if check_cnt == window:
+                ans.append(i)
+        return ans

7b57233a8d001d71a3ab588b225562c19165eeb8
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-06-09 19:44:20 -0700

    Time: 41 ms (70.77%), Space: 13.9 MB (99.64%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -2,13 +2,12 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
         def backtrack(subset: List[int],  start: int):
-            #if subset not in res:
+            # this algorithm will only add unique combos
             subset_copy = subset.copy()
             res.append(subset_copy)
             for i in range(start, len(nums)):
                 subset.append(nums[i])
                 backtrack(subset, i+1)
                 subset.pop()
-        #res.append([])
         backtrack([], 0)
         return res

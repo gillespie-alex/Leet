4f324cdeaf1d188607ce10bbae971a4e604ef7c5
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-06-18 15:38:26 -0700

    Time: 73 ms (84.72%), Space: 14.7 MB (71.88%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,9 +1,8 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        # using a min-heap, so to get the largest value we want the the inverse kth element
         if not nums:
             return
-        # currently a min-heap so to get the largest value we want the the inverse kth element
-        # res holds the correct index in the min-heap to return
         res = len(nums) - k
         heapq.heapify(nums)
         print(nums)

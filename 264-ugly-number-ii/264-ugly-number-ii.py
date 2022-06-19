4876202e7bfa16d753398bc8718f711a5d8485ae
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-06-18 23:10:31 -0700

    Time: 370 ms (23.38%), Space: 14.2 MB (10.24%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.py
+++ 264-ugly-number-ii/264-ugly-number-ii.py
@@ -2,21 +2,14 @@ class Solution:
     def nthUglyNumber(self, n: int) -> int:
         ugly_nums = set()
         ugly_nums.add(1)
+        factors = [2,3,5]
         heap = [1]    
         for i in range(1,n):
             multiplier = heapq.heappop(heap)
-            x = 2*multiplier
-            y = 3*multiplier
-            z = 5*multiplier
-            if x not in ugly_nums:
-                ugly_nums.add(x)
-                heapq.heappush(heap,x)
-            if y not in ugly_nums:
-                ugly_nums.add(y)
-                heapq.heappush(heap,y)
-            if z not in ugly_nums:
-                ugly_nums.add(z)
-                heapq.heappush(heap,z)    
+            for uglies in factors:
+                if uglies*multiplier not in ugly_nums:
+                    ugly_nums.add(uglies*multiplier)
+                    heapq.heappush(heap,uglies*multiplier)
         return heapq.heappop(heap)
 
         

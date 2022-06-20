38308bb3112869705cc272eda7e3bd6f7bc69356
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-06-19 22:28:25 -0700

    Time: 42 ms (62.47%), Space: 13.9 MB (69.41%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,9 +1,5 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # use a stack appeding the inputs and popping only when most recent input 
-        # is the correct inverse of the next incoming input
-        # the queue must be empty in the end for it to be valid
-        # answer is in O(n) time complexity and O(n) space complexity
         string = []
         string.extend(s)
         stack = deque()

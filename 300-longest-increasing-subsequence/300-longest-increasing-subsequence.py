fae70a8f09720ca6b6b09b36637c9fb10df4e8ac
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-06-25 15:40:08 -0700

    Time: 5604 ms (27.55%), Space: 14.3 MB (46.20%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,21 +1,17 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # helper function used to clean code and accomplishes the following:
+        # looks for all possible previous subsequences that we can add the current dp[i] to
+
         def helper(end_range:int, dp_list: List[int]):
-            possibilities = []
+            subs = 1
             for i in range(end_range):
-                # if the number for a specific dp is less than the current end_range's val then we add to subsequence
-                # nums[i] vs nums[end_range]
                 if nums[end_range] > nums[i]:
-                    # if this is true update the max subsequence at this dp position
-                    possibilities.append(1 + dp_list[i])
-            return 1 if not possibilities else max(possibilities)
-
-
-
-            # compare the dp indices if they are valid to become subsequence
+                    subs = max(subs,1 + dp_list[i])
+            return subs
+        
         if not nums:
             return 0
-        # every number of the array will have at least a subsequence of 1
         dp = [1] * len(nums)
         for i in range(1,len(nums)):
             dp[i] = helper(i,dp)

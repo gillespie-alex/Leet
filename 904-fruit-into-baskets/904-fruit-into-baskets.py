1686804150472dcac07e3950f2d5caa2a8aac71d
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-07-07 15:11:22 -0700

    Time: 1664 ms (23.35%), Space: 20.5 MB (17.30%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,25 +1,5 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        # if not fruits:
-        #     return 0
-        # # this question essentially boils down to the longest contiguous sub-array  
-        # l,r = 0,0
-        # baskets = set()
-        # baskets.add(fruits[0])
-        # Max = 1
-        # while r < len(fruits) - 1:
-        #     r += 1
-        #     baskets.add(fruits[r])
-        #     while len(baskets) > 2:
-        #         prev = l
-        #         l += 1
-        #         if fruits[prev] in fruits[l:r]:
-        #             continue
-        #         else:
-        #             baskets.remove(fruits[prev])
-        #     Max = max(Max, r-l+1)
-        #     #Max = max(Max, len(fruits[l:r+1]))
-        # return Max
         if not fruits:
             return 0
         # this question essentially boils down to the longest contiguous sub-array  
@@ -37,11 +17,3 @@ class Solution:
                 l += 1
             Max = max(Max, r - l + 1)
         return Max
-        # count, i = {}, 0
-        # for j, v in enumerate(fruits):
-        #     count[v] = count.get(v, 0) + 1
-        #     if len(count) > 2:
-        #         count[fruits[i]] -= 1
-        #         if count[fruits[i]] == 0: del count[fruits[i]]
-        #         i += 1
-        # return j - i + 1

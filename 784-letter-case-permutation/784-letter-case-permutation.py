4a672d5a6d4081f6b6964391880c5dab5be20c53
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-07-27 18:27:10 -0700

    Time: 90 ms (53.35%), Space: 15.5 MB (21.24%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        res = []
+        def dfs(index, sub):
+            print(index, sub)
+            if len(sub) == len(s):
+                res.append(sub)
+                return
+            # for i in range(index, len(s)):
+            if s[index] in string.ascii_letters:
+                dfs(index + 1, sub + s[index].lower())
+                dfs(index + 1, sub + s[index].upper())
+            else:
+                dfs(index + 1, sub + s[index])
+                    
+        dfs(0, "")
+        return res

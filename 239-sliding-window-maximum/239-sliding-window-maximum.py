5cfa06f39eb0107f4acc452e5cefa421d282c52e
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-07-30 16:43:52 -0700

    Time: 2264 ms (77.89%), Space: 30.3 MB (24.92%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # Use a monotonic decreasing queue
+        # O(n) solution using a monotonic decreasing queue
         dec_queue = []
         # fill up the queue for the first window
         for i in range(k):
@@ -11,7 +11,6 @@ class Solution:
         res.append(nums[dec_queue[0]])
         for i in range(1, len(nums) - k + 1):
             new_val = nums[i + k - 1]
-            #print(f"{new_val} and {dec_queue}")
             while dec_queue and new_val > nums[dec_queue[-1]]:
                 dec_queue.pop()
             dec_queue.append(i + k - 1)

5af4495b459fc3ae0c25c780c6c31ca76fc3978e
gillespie-alex <102345712+gillespie-alex@users.noreply.github.com>
2022-08-06 16:48:01 -0700

    Time: 132 ms (94.91%), Space: 14.8 MB (88.68%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,17 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        def bin_search(arr, t):
-            left, right = 0, len(arr) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if arr[mid] == t:
-                    return mid
-                if arr[mid] < t:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return -999
-        for i, val in enumerate(nums):
-            temp = bin_search(nums[i+1:], target - val)
-            if temp != -999:
-                return [i+1,temp+i+2]
+        l, r = 0, len(nums) - 1
+        while l < r:
+            allowed = target - nums[l]
+            while nums[r] > allowed:
+                r -= 1
+            if nums[r] == allowed:
+                return [l+1, r+1]
+            l += 1
